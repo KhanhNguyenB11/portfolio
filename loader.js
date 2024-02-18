e4eb9a0ed4ee69b29d6cbd8a798846074a3ccd75
KhanhNguyenB11 <dantedmc1134@gmail.com>
2024-02-18 21:12:49 +0700

    deploy

---
+++ loader.js
@@ -0,0 +1,12 @@
+'use client'
+const externalHost = ["cdn-icons-png.flaticon.com", "upload.wikimedia.org", "w7.pngwing.com", "images.pexels.com"]
+export default function myImageLoader({ src, width, quality }) {
+  // Check if src starts with any of the external hosts
+  const isExternal = externalHost.some(host => src.startsWith(`https://${host}`) || src.startsWith(`http://${host}`));
+  if (isExternal) {
+      return src; // Return the src URL as is if it starts with any of the external hosts
+  } else {
+      // Modify the src URL by adding width and quality parameters
+      return `https://khanhportfolio.com/${src}?w=${width}&q=${quality || 75}`;
+  }
+}
