366a2e275e7e430893817e86b1d99e71ec92be04
KhanhNguyenB11 <dantedmc1134@gmail.com>
2024-03-05 21:51:53 +0700

    added CV download

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -30,7 +30,7 @@ const Homepage = () => {
         {/* buttons */}
         <div className="flex gap-4 ">
           <button className="relative flex h-[50px] w-40 rounded items-center justify-center overflow-hidden bg-black text-white shadow-2xl transition-all before:absolute before:h-0 before:w-0 before:rounded-full before:bg-white before:duration-500 before:ease-out hover:shadow-white hover:text-black hover:before:h-56 hover:before:w-56">
-            <Link href="/portfolio" className="relative z-2">View my work</Link>
+            <a href="/NguyenTuanAnhKhanh_fullstack.pdf" className="relative z-2" download={"KhanhNguyen CV"}>Download CV</a> 
           </button>
           <button className="relative flex h-[50px] w-40 rounded items-center justify-center overflow-hidden bg-black text-white shadow-2xl transition-all before:absolute before:h-0 before:w-0 before:rounded-full before:bg-white before:duration-500 before:ease-out hover:shadow-white hover:text-black hover:before:h-56 hover:before:w-56">
             <Link href="/contact" className="relative z-2">Contact me</Link>
