1bda371beeab7f5fc4eea60339225cc414e42ec8
KhanhNguyenB11 <dantedmc1134@gmail.com>
2024-03-10 20:17:23 +0700

    fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
+    assetPrefix: "./",
     images:{
         remotePatterns:[{
             protocol: 'https',
